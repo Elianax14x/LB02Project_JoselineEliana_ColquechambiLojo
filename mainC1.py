7418f99016d841b3d3eb111774a069479b2e6f0c
Elianax14x <colquechambij@bzz.ch>
2023-11-13 00:02:59 +0100

    DONE

--- mainC1.py
+++ mainC1.py
@@ -2,7 +2,6 @@ from flask import Flask, request, session
 
 app = Flask(__name__)
 
-
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
@@ -12,3 +11,6 @@ def login():
 
     session['logged_in'] = True
     return 'Erfolgreich eingeloggt!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
